941bba7e137c0f7f75deb12bc5ce2ee4f9b371ad
Martin Palko <jeeprubi@hotmail.com>
2018-01-26 22:49:59 -0500

    Whoops, wrong thing pasted in wrong file.

--- Assets/Game/Scripts/Flightpath.cs
+++ Assets/Game/Scripts/Flightpath.cs
@@ -2,47 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aeroplane : MonoBehaviour
+public sealed class Flightpath : MonoBehaviour
 {
-	private float _yaw;
-	private float _pitch;
-	private float _roll;
-	private float _throttle;
-
-	public float yaw
+	public struct Waypoint
 	{
-		get { return _yaw; }
-		set { _yaw = Mathf.Clamp(value, -1.0f, 1.0f); }
+		Vector3 position;
 	}
 
-	public float pitch
-	{
-		get { return _pitch; }
-		set { _pitch = Mathf.Clamp(value, -1.0f, 1.0f); }
-	}
-	public float roll
-	{
-		get { return _roll; }
-		set { _roll = Mathf.Clamp(value, -1.0f, 1.0f); }
-	}
-	public float throttle
-	{
-		get { return _throttle; }
-		set { _throttle = Mathf.Clamp(value, 0.0f, 1.0f); }
-	}
+	private LinkedList<Waypoint> _waypoints = new LinkedList<Waypoint>();
 
-
-	[SerializeField]
-	protected float thrustPower;
-	[SerializeField]
-	protected float mass;
-	[SerializeField]
-	protected float drag;
-	[SerializeField]
-	protected float manouverability;
-
-	protected void FixedUpdate()
+	public Flightpath(Flightpath.Waypoint initialWaypoint)
 	{
-
+		_waypoints.AddFirst(initialWaypoint);
 	}
 }
