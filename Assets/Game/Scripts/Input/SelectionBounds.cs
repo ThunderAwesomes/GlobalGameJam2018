04eca49c0f84e93258a3277900fd2e29fb21f2a4
ByronMayne <ByronMayne@gmail.com>
2018-01-27 14:49:39 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Game/Scripts/Input/SelectionBounds.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SelectionBounds : MonoBehaviour
+{
+	private BoxCollider _boxCollider;
+
+	// Use this for initialization
+	public void Start()
+	{
+		_boxCollider = GetComponent<BoxCollider>();
+		_boxCollider.isTrigger = true;
+		CalculateBounds();
+	}
+
+	/// <summary>
+	/// Calculates the bounds around an object 
+	/// </summary>
+	public void CalculateBounds()
+	{
+		Collider[] colliders = GetComponentsInChildren<Collider>();
+
+		bool hasBounds = false;
+		Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+
+		for (int i = 0; i < colliders.Length; ++i)
+		{
+			if (hasBounds)
+			{
+				bounds.Encapsulate(colliders[i].bounds);
+			}
+			else
+			{
+				bounds = colliders[i].bounds;
+				hasBounds = true;
+			}
+		}
+
+		_boxCollider.center = bounds.center - transform.position;
+		_boxCollider.size = bounds.size;
+	}
+}
