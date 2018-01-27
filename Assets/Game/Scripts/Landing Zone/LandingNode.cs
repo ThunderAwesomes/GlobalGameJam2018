596e7ea22bfbee6a1738d043b3e5e4bc2b84f65d
ByronMayne <ByronMayne@gmail.com>
2018-01-27 18:44:35 -0500

    More work

---
+++ Assets/Game/Scripts/Landing Zone/LandingNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class LandingNode : MonoBehaviour
+{
+	[SerializeField]
+	private int _id;
+
+	private BoxCollider _collider;
+
+	public BoxCollider boxCollider
+	{
+		get
+		{
+			if(_collider == null)
+			{
+				_collider = GetComponent<BoxCollider>();
+			}
+			return _collider;
+		}
+	}
+
+	
+	public int id
+	{
+		get { return _id; }
+		set { _id = value; }
+	}
+}
---
+++ Assets/Game/Scripts/Landing Zone/LandingZone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingZone : MonoBehaviour
+{
+	[SerializeField]
+	private float _length;
+
+	[SerializeField]
+	private float _pathBreakSize = 0.6f;
+
+	[SerializeField]
+	private BoxCollider _leftWall;
+	[SerializeField]
+	private BoxCollider _rightWall;
+	[SerializeField]
+	private BoxCollider _ceiling; 
+	
+	[SerializeField]
+	private List<LandingNode> _landingNodes;
+	[SerializeField]
+	private Vector3 _colliderSize = new Vector3(16, 3, 3);
+
+	private void OnValidate()
+	{
+		for(int i = 0; i < _landingNodes.Count; i++)
+		{
+			_landingNodes[i].boxCollider.size = _colliderSize;
+			_landingNodes[i].boxCollider.center = new Vector3(0, _colliderSize.y / 2f, 0);
+			_landingNodes[i].id = i;
+			_landingNodes[i].name = "Landing Trigger " + i.ToString();
+		}
+		float halfWidth = _colliderSize.x / 2f;
+		float wallHeight = _colliderSize.y + _pathBreakSize;
+		float wallCenter = (wallHeight / 2f);
+
+		if (_leftWall != null)
+		{
+			_leftWall.size = new Vector3(_pathBreakSize, wallHeight, _length);
+			_leftWall.center = new Vector3(-halfWidth - (_leftWall.size.x * .5f), wallCenter, _length / 2f);
+		}
+
+		if (_rightWall != null)
+		{
+			_rightWall.size = new Vector3(_pathBreakSize, wallHeight, _length);
+			_rightWall.center = new Vector3(halfWidth + (_rightWall.size.x * .5f), wallCenter, _length / 2f);
+		}
+
+		if(_ceiling != null)
+		{
+			_ceiling.center = new Vector3(0f, _colliderSize.y + (0.5f * _pathBreakSize), _length / 2f);
+			_ceiling.size = new Vector3(_colliderSize.x, _pathBreakSize, _length);
+		}
+	}
+}
