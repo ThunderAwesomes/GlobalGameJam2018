7bb0f42f667d8a285cc50d4840d8770c3c976f55
ByronMayne <ByronMayne@gmail.com>
2018-01-26 22:23:44 -0500

    Added the Main Menu Scene
    Created the Game Mode and Input Manager

---
+++ Assets/Game/Scripts/GameMode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    [SerializeField]
+    private VRInputManager _inputManagerPrefab;
+
+
+    private VRInputManager _inputManager;
+
+    private void Awake()
+    {
+        StartMode();
+    }
+
+    public void StartMode()
+    {
+        CreateInputManager();
+    }
+
+    public void EndMode()
+    {
+        if(_inputManager != null)
+        {
+            Destroy(_inputManager);
+        }
+    }
+
+    private void CreateInputManager()
+    {
+        _inputManager = Instantiate<VRInputManager>(_inputManagerPrefab);
+        _inputManager.transform.SetParent(transform);
+        _inputManager.name = _inputManagerPrefab.name;
+    }
+}
